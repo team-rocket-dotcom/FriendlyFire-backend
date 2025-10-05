952f4558190f901f3f07e01ac92d7f9de49d4bc5
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-10-05 18:17:06 +0530

    initial commit

---
+++ friendly_fire/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for friendly_fire project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'friendly_fire.settings')
+
+application = get_wsgi_application()
