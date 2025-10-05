6d73e4dcef312a80b7a41a6b2ff8575c803a0c61
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-10-05 18:55:29 +0530

    feat(tokens): implement token generation for active users

---
+++ accounts/tokens.py
@@ -0,0 +1,10 @@
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import AuthenticationFailed
+
+def get_tokens_for_user(user):
+    if not user.is_active:
+        raise AuthenticationFailed("User is not active")
+    
+    refresh_token = RefreshToken.for_user(user)
+
+    return str(refresh_token),str(refresh_token.access_token)
