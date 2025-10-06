d0152a49fe2fe86ce4445d6e9189319274606edf
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-10-06 15:26:24 +0530

    fix(urls): correct URL patterns for authentication views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import SignInView, SignUpView, GoogleOAuthView, CustomTokenRefreshView
 
 urlpatterns = [
-    path('sign-up/',),
-    path('sign-in/',),
-    path('sign-out/',)
+    path('sign-up',SignUpView.as_view(), name='sign-up'),
+    path('sign-in',SignInView.as_view(), name='sign-in'),
+    path('google',GoogleOAuthView.as_view(), name='google-oauth'),
+    path('token/refresh', CustomTokenRefreshView.as_view(), name='token-refresh')
 ]
